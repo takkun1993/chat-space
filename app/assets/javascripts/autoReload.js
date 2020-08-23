673506eb057736f887f84a651afe9901cae1904f
takkun1993 <68512825+takkun1993@users.noreply.github.com>
2020-08-23 19:10:39 +0900

    Update autoReload.js

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -39,7 +39,8 @@ $(function(){
   }
   let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.chat-main__message-form:last').data("message-id") || 0;
+    let last_message_id = $('.chat-main__message-list__info:last').data("message-id") || 0;
+    console.log(last_message_id)
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
